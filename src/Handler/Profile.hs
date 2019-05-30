5e323695bca06e3b91b2ff0a7e5fbc5fd5747f3e
Manuel Ospina <manuel@anect.jp>
2019-05-30 23:29:46 +0900

    Add profile model

--- src/Handler/Profile.hs
+++ src/Handler/Profile.hs
@@ -8,26 +8,27 @@ module Handler.Profile where
 import Import
 import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3, bfs)
 
-data Profile = Profile { name :: Text }
+-- data Profile = Profile { name :: Text }
 
-profileForm :: AForm Handler Profile
-profileForm = Profile
+profileForm :: UserId -> AForm Handler Profile
+profileForm userId = Profile
     <$> areq textField (bfs ("Profile Name" :: Text)) Nothing
+    <*> pure userId
 
 getProfileR :: Handler Html
 getProfileR = do
-    (_, user) <- requireAuthPair
-    (formWidget, formEnctype) <- generateFormPost $ renderBootstrap3 BootstrapBasicForm profileForm
-    let profileName = Nothing :: Maybe Profile
+    (uid, user) <- requireAuthPair
+    (formWidget, formEnctype) <- generateFormPost $ renderBootstrap3 BootstrapBasicForm $ profileForm uid
+    let maybeProfile = Nothing :: Maybe Profile
     defaultLayout $ do
         setTitle . toHtml $ userEmail user <> "'s User page"
         $(widgetFile "profile")
 
 postProfileR :: Handler Html
 postProfileR = do
-    (_, user) <- requireAuthPair
-    ((res, formWidget), formEnctype) <- runFormPost $ renderBootstrap3 BootstrapBasicForm profileForm
-    let profileName = case res of
+    (uid, user) <- requireAuthPair
+    ((res, formWidget), formEnctype) <- runFormPost $ renderBootstrap3 BootstrapBasicForm $ profileForm uid
+    let maybeProfile = case res of
             FormSuccess profile -> Just profile
             _ -> Nothing
     defaultLayout $ do
