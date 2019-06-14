48564fd4604292b3afaf23beba0553deaba5fbd3
Manuel Ospina <manuel@anect.jp>
2019-06-14 16:03:27 +0900

    Add restriction to view parts of the page to anonymous users.

--- src/Handler/Programs.hs
+++ src/Handler/Programs.hs
@@ -9,9 +9,15 @@ import Import
 
 getProgramsR :: Text -> Handler Html
 getProgramsR profile = do
+    muser <- maybeAuthPair
+    let mProfileOwner = maybeProfileOwner profile muser
     defaultLayout $ do
         setTitle . toHtml $ profile
         $(widgetFile "programs/index")
 
 postProgramsR :: Text -> Handler Html
 postProgramsR profile = error "Not yet implemented: postProgramsR"
+
+maybeProfileOwner :: Text -> Maybe (UserId, User) -> Maybe Profile
+maybeProfileOwner _ Nothing = Nothing
+maybeProfileOwner profile (Just (uid, user)) = undefined 
