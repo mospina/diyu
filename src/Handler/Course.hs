41f4caf3de3f9f6ce218bf29836d11b371ebe723
Manuel Ospina <manuel@anect.jp>
2019-07-01 08:22:47 +0900

    Add route for course details

---
+++ src/Handler/Course.hs
@@ -0,0 +1,9 @@
+module Handler.Course where
+
+import Import
+
+getCourseR :: Text -> Text -> Text -> Handler Html
+getCourseR text text text = error "Not yet implemented: getCourseR"
+
+postCourseR :: Text -> Text -> Text -> Handler Html
+postCourseR text text text = error "Not yet implemented: postCourseR"
---
+++ test/Handler/CourseSpec.hs
@@ -0,0 +1,14 @@
+module Handler.CourseSpec (spec) where
+
+import TestImport
+
+spec :: Spec
+spec = withApp $ do
+
+    describe "getCourseR" $ do
+        error "Spec not implemented: getCourseR"
+
+
+    describe "postCourseR" $ do
+        error "Spec not implemented: postCourseR"
+
