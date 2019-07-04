576efa0c91a2fdf1c4eee5808a371eb6f857f681
Manuel Ospina <manuel@anect.jp>
2019-07-05 08:07:49 +0900

    Add Article endpoint

--- src/Application.hs
+++ src/Application.hs
@@ -46,6 +46,7 @@ import Handler.Comment
 import Handler.Profile
 import Handler.Programs
 import Handler.Course
+import Handler.Article
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
---
+++ src/Handler/Article.hs
@@ -0,0 +1,6 @@
+module Handler.Article where
+
+import Import
+
+getArticleR :: Text -> Text -> Text -> Text -> Handler Html
+getArticleR text text text text = error "Not yet implemented: getArticleR"
